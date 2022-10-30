4ffb5aa2a16822ebb0e06ad63e7f4b87e7e39f6a
Kofi K <kofivincent026@gmail.com>
2022-10-30 19:46:10 +0000

    2 modified end of destructing

--- DecorationCodelab/src/main/kotlin/Decoration.kt
+++ DecorationCodelab/src/main/kotlin/Decoration.kt
@@ -14,3 +14,20 @@ fun makeDecorations() {
     println (decoration1.equals(decoration2))
     println (decoration3.equals(decoration2))
 }
+
+data class Decoration2(val rocks: String, val wood: String, val diver: String){
+}
+
+fun makeDecorations2() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+// Assign all properties to variables.
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+
+    val d6 = Decoration2("crystal", "wood", "diver")
+    val (rock6, _, diver6) = d6
+}
