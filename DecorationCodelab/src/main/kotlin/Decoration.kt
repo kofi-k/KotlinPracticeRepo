cc2d47614c3f58204919785cd8e6c48b47b769bd
Kofi K <kofivincent026@gmail.com>
2022-10-30 19:53:11 +0000

    2 modifications
    end of enums

--- DecorationCodelab/src/main/kotlin/Decoration.kt
+++ DecorationCodelab/src/main/kotlin/Decoration.kt
@@ -31,3 +31,10 @@ fun makeDecorations2() {
     val d6 = Decoration2("crystal", "wood", "diver")
     val (rock6, _, diver6) = d6
 }
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
--- DecorationCodelab/src/main/kotlin/Main.kt
+++ DecorationCodelab/src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 fun main() {
     //makeDecorations()
-    makeDecorations2()
+    //makeDecorations2()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+
+    println(Direction.NORTH.name)
+    println(Direction.NORTH.ordinal)
+    println(Direction.NORTH.degrees)
+
 }
