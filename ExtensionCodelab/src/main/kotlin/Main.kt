36a097feb72277435fe594e2f2590b6bcdadacd0
Kofi K <kofivincent026@gmail.com>
2022-10-31 10:42:10 +0000

    8 files added 2 modified

---
+++ ExtensionCodelab/src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main(args: Array<String>) {
+    println("Does it have spaces?".hasSpaces())
+
+    val plant = GreenLeafyPlant(size = 10)
+    plant.print()
+    println(plant.isGreen)
+    println("\n")
+    val aquariumPlant: AquariumPlant = plant
+    aquariumPlant.print()  // what will it print?
+    aquariumPlant.isGreen
+}
+fun String.hasSpaces(): Boolean {
+    val found = this.find { it == ' ' }
+    return found != null
+}
+
+//fun String.hasSpaces() = find { it == ' ' } != null //simplified
+
+fun spaceToUnderscore (text: String) {
+      var under =  text.forEach { if (it.isWhitespace())  '_' }
+    println(under)
+}
+
+open class AquariumPlant(val color: String, private val size: Int)
+
+class GreenLeafyPlant(size: Int) : AquariumPlant("green", size)
+
+//extension functions
+fun AquariumPlant.print() = println("AquariumPlant")
+fun GreenLeafyPlant.print() = println("GreenLeafyPlant")
+
+//extension property
+val AquariumPlant.isGreen: Boolean
+    get() = color == "green"
+
+fun AquariumPlant?.pull() {
+    this?.apply {
+        println("removing $this")
+    }
+}
+
+
