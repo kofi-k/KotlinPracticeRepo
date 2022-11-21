60b093aa1d31157fb3175a695c679ef729d0ade2
Kofi K <kofivincent026@gmail.com>
2022-11-21 11:25:48 +0000

    1 added file 1 modified file

---
+++ Acquarium_Generics/src/main/kotlin/generics/Aquarium.kt
@@ -0,0 +1,36 @@
+package generics
+
+open class WaterSupply(var needsProcessing: Boolean)
+
+class TapWater : WaterSupply(true) {
+    fun addChemicalCleaners() {
+        needsProcessing = false
+    }
+}
+
+class FishStoreWater : WaterSupply(false)
+
+class LakeWater : WaterSupply(true) {
+    fun filter() {
+        needsProcessing = false
+    }
+}
+
+class Aquarium<T: WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        check(!waterSupply.needsProcessing) { "water supply needs processing first" }
+        println("adding water from $waterSupply")
+    }
+}
+
+    fun genericsExample() {
+    val aquarium = Aquarium(waterSupply = TapWater())
+    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
+    aquarium.waterSupply.addChemicalCleaners()
+    println("water needs processing: ${aquarium.waterSupply.needsProcessing}")
+
+    val aquarium2 = Aquarium(LakeWater())
+        aquarium2.waterSupply.filter()
+    println(aquarium2.waterSupply)
+        aquarium2.addWater()
+}
