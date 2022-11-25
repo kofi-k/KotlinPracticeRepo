b88dcc030d9f426656632fd99cbf8f0b365cc952
Kofi K <kofivincent026@gmail.com>
2022-11-25 01:34:10 +0000

    1 modified file

--- SynchronousCode/src/main/kotlin/Main.kt
+++ SynchronousCode/src/main/kotlin/Main.kt
@@ -1,14 +1,24 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    runBlocking {
-        println("Weather forecast")
-        printForecast()
+    val time = measureTimeMillis {
+        runBlocking {
+            println("Weather forecast")
+            printForecast()
+            printTemperature()
+        }
     }
+    println("Execution time: ${time / 1000.0} seconds")
 }
 
 suspend fun printForecast() {
-    delay(2000)
+    delay(1000)
     println("Sunny")
+}
+
+suspend fun printTemperature() {
+    delay(1000)
+    println("30\u00b0C")
 }
